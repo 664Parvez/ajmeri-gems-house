6d4c3cda01e940e4d60a216244557edfeef4a648
664Parvez <parvezrabbi664@gmail.com>
2025-09-02 11:52:01 +0600

    update

---
+++ public/js/app.js
@@ -0,0 +1,18 @@
+const offerImg = document.querySelectorAll("#offer_img")
+const popup = document.getElementById("popup")
+const popupImg = document.getElementById("popup_img")
+
+offerImg.forEach((items) => {
+    items.addEventListener("click", () => {
+        popupImg.src = items.src
+        popup.style.display = "flex"
+        console.log(items.src)
+    })
+})
+
+const popupClose = document.getElementById("popup_close")
+
+popupClose.addEventListener("click", () => {
+    popup.style.display = "none"
+})
+
